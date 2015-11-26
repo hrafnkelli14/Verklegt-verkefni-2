44954a82a83a14009c82938df6cf086e898949b6
hrafnkelli14 <hrafnkelli14@ru.is>
2015-11-26 16:16:05 +0000

    xml file class for data layer

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
 #include <QCoreApplication>
+#include "xmlfile.h"
+
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+
+    xmlfile newxml("data");
+    //newxml.addPerson("Alan Turing"); //Adds new person to xml with the name Alan Turing
 
     return a.exec();
 }
